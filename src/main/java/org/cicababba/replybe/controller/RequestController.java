fbca8004fcc08e0a93ec2c2b054e7f2a5cc24497
Matteo Adduci <matteo.adduci94@gmail.com>
2025-07-16 02:42:40 +0200

    added post api and http session to manage a fake db

--- src/main/java/org/cicababba/replybe/controller/RequestController.java
+++ src/main/java/org/cicababba/replybe/controller/RequestController.java
@@ -1,22 +1,41 @@
 package org.cicababba.replybe.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.cicababba.replybe.model.Request;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
 public class RequestController {
 
     @GetMapping("/request")
-    public List<Request>  getRequests() {
-        List<Request> requests = new ArrayList<Request>();
-        requests.add(new Request("Titolo", "Descrizione", new Date()));
+    public List<Request>  getRequests(HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<Request> requests = (List<Request>) session.getAttribute("REQUESTS");
+        if (requests == null) {
+            requests = new ArrayList<>();
+            session.setAttribute("REQUESTS", requests);
+        }
+        System.out.println(requests);
         return requests;
+    }
+
+    @PostMapping("/request")
+    public String newRequest(@RequestBody Request request, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<Request> requests = (List<Request>) session.getAttribute("REQUESTS");
+        if(Objects.isNull(requests)) {
+            requests = new ArrayList<>();
+        }
+        request.setCreationDate(new Date());
+        requests.add(request);
+        session.setAttribute("REQUESTS", requests);
+        System.out.println(requests);
+        return "OK!";
     }
 }
